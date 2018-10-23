f748daa6a0b1394ebfd9c6a4b545afeb1a326b37
ana-kuznetsova <menina.indigena.17@gmail.com>
2018-10-23 23:11:54 +0300

    Finish draft for main module

--- api_modules/ner_module/parse_ner.py
+++ api_modules/ner_module/parse_ner.py
@@ -1,7 +1,19 @@
 from bootstrap_ner import parse_user_text
 from launch_tomita import detect_ner_tomita
+import re
 
-def extract_ner(user_text: str) -> str:
+print('Start')
+def extract_ner(user_text: str) -> list:
     detect_ner_tomita(user_text)
-    return parse_user_text()
-        
+    names = parse_user_text(user_text)
+    return names
+
+def markup_ner(user_text: str) -> None:
+    def tag_names(user_text: str, names:list):
+        search_pattern = '|'.join(names)
+        replace_pattern = '<span class="name">\g<0></span>'
+        return re.sub(search_pattern, replace_pattern, user_text)
+
+    detect_ner_tomita(user_text)
+    names = parse_user_text(user_text)
+    return tag_names(user_text, names)
